7cbbd9dee7bb5c52a4f4eb5724d0a4374a6f1215
fraissatt <jvalencarmamede@gmail.com>
2026-09-13 16:14:46 -0300

    Resultado da aprovação do aluno com método próprio

--- Sistema.java
+++ Sistema.java
@@ -4,18 +4,19 @@ public class Sistema {
         return (nota1 + nota2) / 2;
     }
 
+    public static String resultadoAluno(double media) {
+        return media >= 6 ? "Aprovado" : "Reprovado";
+    }
+
     public static void main(String[] args) {
         String nome = "Carlos";
         double nota1 = 8;
         double nota2 = 7;
         double media = mediaAluno(nota1, nota2);
+        String resultado = resultadoAluno(media);
 
         System.out.println("Aluno: " + nome);
         System.out.println("Media: " + media);
-        if (media >= 6) {
-            System.out.println("Aprovado");
-        } else {
-            System.out.println("Reprovado");
-        }
+        System.out.println(resultado);
     }
 }
