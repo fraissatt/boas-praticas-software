f3763dafd2bd600329b578fd9a8567a71f296f53
fraissatt <jvalencarmamede@gmail.com>
2026-09-13 16:13:59 -0300

    Cálculo da média com método próprio

--- Sistema.java
+++ Sistema.java
@@ -1,9 +1,15 @@
 public class Sistema {
+
+    public static double mediaAluno(double nota1, double nota2) {
+        return (nota1 + nota2) / 2;
+    }
+
     public static void main(String[] args) {
         String nome = "Carlos";
         double nota1 = 8;
         double nota2 = 7;
-        double media = (nota1 + nota2) / 2;
+        double media = mediaAluno(nota1, nota2);
+
         System.out.println("Aluno: " + nome);
         System.out.println("Media: " + media);
         if (media >= 6) {
