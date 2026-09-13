7b92ab59be27277c31ce10e697164e150ea3ca09
fraissatt <jvalencarmamede@gmail.com>
2026-09-13 16:16:47 -0300

    Saída padronizada e versão final do código

--- Sistema.java
+++ Sistema.java
@@ -8,15 +8,15 @@ public class Sistema {
         return media >= 6 ? "Aprovado" : "Reprovado";
     }
 
-    public static void main(String[] args) {
+    // main é somente utilizada para orquestrar o fluxo + saída
+    public static void main (String[] args) {
         String nome = "Carlos";
-        double nota1 = 8;
-        double nota2 = 7;
+        double nota1 = 7.5;
+        double nota2 = 8.0;
+
         double media = mediaAluno(nota1, nota2);
         String resultado = resultadoAluno(media);
 
-        System.out.println("Aluno: " + nome);
-        System.out.println("Media: " + media);
-        System.out.println(resultado);
-    }
+        System.out.println("Aluno: " + nome + "\n - Média: " + media + "\n - Resultado: " + resultado);
+    } 
 }
